839084f7ef83861865a0228083dc829c54183b6d
Jessica Antunes <jessantunes@protonmail.com>
2021-02-26 10:50:16 -0500

    commit tree a84880fe7c8022b4829f89b11eb7595213c97da4

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+    array.sort { | a, b | a <=> b }
+end
 
+def sort_array_desc(array)
+    array.sort { | a, b | b <=> a }
+end
+
+def sort_array_char_count(array)
+    array.sort { | a, b | a.length <=> b.length }
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)   
+    array.each { |word| word[2] = "$" }
+end
+
+def find_a(array)
+array.select {|word| word.start_with?("a")}
+end
+
+def sum_array(array)
+sum = 0
+array.collect { |number| sum += number}
+sum
+end
+
+def add_s(array)
+    array.collect.with_index do |word, i| 
+        if i != 1
+            word<<("s")
+        else
+            word
+        end
+    end
+end
